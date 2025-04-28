4a85f259933b73bd14aa676b0d118ffce8d4d686
riclanzoni <riclanzoni@gmail.com>
2025-04-28 10:12:26 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'screens/admin_list_screen.dart';
+import 'screens/admin_edit_screen.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FleetMaster',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const AdminListScreen(),
+        '/admin_edit': (context) => const AdminEditScreen(),
+      },
+    );
+  }
+}
